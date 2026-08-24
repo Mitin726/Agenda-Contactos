ec0e430c87af4d7c9170c079e3e23253003f2e3c
JuanCarvajal28 <jmartincg28@gmail.com>
2026-08-24 11:06:53 -0500

    FEAT: Primera versión menu.

--- AgendaContactos.java
+++ AgendaContactos.java
@@ -1,25 +1,84 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AgendaContactos{
 
     private ArrayList<Contacto> contactos;
+    private Scanner scanner;
 
     public AgendaContactos(){
         contactos = new ArrayList<>();
+        scanner = new Scanner(System.in);
     }
 
-    public void Iniciar(){
-        System.out.println("Agenda de Contactos");
-        mostrarMenu();
+    public void iniciar() {
+
+        int opcion;
+
+        do {
+            mostrarMenu();
+            System.out.print("Seleccione una opción: ");
+            try {
+                opcion = Integer.parseInt(scanner.nextLine());
+
+            } catch (NumberFormatException e) {
+                System.out.println("Opción inválida. Debe ingresar un número.");
+                opcion = 0;
+            }
+
+            switch (opcion) {
+                case 0: 
+                    break;
+                case 1:
+                    System.out.println("Agregar contacto");
+                    break;
+
+                case 2:
+                    System.out.println("Ver todos los contactos");
+                    break;
+
+                case 3:
+                    System.out.println("Buscar contacto");
+                    break;
+
+                case 4:
+                    System.out.println("Eliminar contacto");
+                    break;
+
+                case 5:
+                    System.out.println("¡Hasta luego!");
+                    break;
+
+                default:
+                    System.out.println("Opción inválida.");
+            }
+
+        } while (opcion != 5);
     }
 
     private void mostrarMenu(){
         System.out.println("""
-            1. Agregar contacto
-            2. Ver todos los contactos
-            3. Buscar contacto
-            4. Eliminar Contacto
-            5. Salir
+        1. Agregar contacto
+        2. Ver todos los contactos
+        3. Buscar contacto
+        4. Eliminar Contacto
+        5. Salir
         """);
+    }
+
+    private void agregarContacto(){
+        
+    }
+
+    private void mostrarContactos(){
+
+    }
+
+    private void buscarContacto(){
+
+    }
+
+    private void eliminarContacto(){
+
     }
 }
